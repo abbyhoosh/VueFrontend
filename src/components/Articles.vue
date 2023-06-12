666396521aceb82a49c08d7dec984a092da438bb
Abby <amhoschouer@wpi.edu>
2023-06-12 15:51:17 -0400

    temporary articles fetch function

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,25 +1,57 @@
 <script>
+const apiBaseURL = 'https://restfulapi--abbyhoosh.repl.co/proxy/5000';
 
 export default {
   data() {
     return {
-      articles: []
+      articles: ''
     };
   },
   methods: {
-    async getALlArticles() {
+    async getAllArticles() {
       const tokenHeader = new Headers();
-      tokenHeader.append('Authorization', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjIwLCJleHAiOjE2ODY2ODEwMzB9.TntY7p6we6A7-9gTfc0sPFizcfEN2JBzmMjBWmjR82I');
-      const apiBaseURL = 'https://restfulapi.abbyhoosh.repl.co/proxy/5000';
-      const data = await fetch(`${apiBaseURL}/articles`,
-          {
-            method: 'GET',
-            headers: tokenHeader
-          });
-      const articleData = data.json();
-      console.log(articleData);
+      tokenHeader.append('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjIwLCJleHAiOjE2ODY2ODEwMzB9.TntY7p6we6A7-9gTfc0sPFizcfEN2JBzmMjBWmjR82I');
+      tokenHeader.append('content-type', 'application/json');
+      try {
+        this.articles = 'enters try';
+        const data = await fetch(`${apiBaseURL}/articles.json`,
+            {
+              method: 'GET',
+              credentials: 'include',
+              headers: {
+                "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjIwLCJleHAiOjE2ODY2ODEwMzB9.TntY7p6we6A7-9gTfc0sPFizcfEN2JBzmMjBWmjR82I',
+                "Content-Type": 'application/json'
+              },
+              mode: 'no-cors'
+            });
+        //this.articles = await data.json();
+      } catch (e) {
+        console.error(e);
+      }
+
     }
   },
 }
-
 </script>
+
+<template>
+  <header>
+    Articles
+  </header>
+
+  <p>
+    {{ articles }}
+  </p>
+
+
+  <button @click="getAllArticles"> get all articles</button>
+
+  <ul>
+  </ul>
+
+</template>
+
+
+<style>
+
+</style>
