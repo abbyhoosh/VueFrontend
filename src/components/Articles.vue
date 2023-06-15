9171e73f792b2eb2ce40f8e188d1c141cad91c03
Abby <amhoschouer@wpi.edu>
2023-06-15 11:05:32 -0400

    workaround article display

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -8,7 +8,7 @@ const apiBaseURL = 'https://restfulapi--abbyhoosh.repl.co/proxy/5000';
 export default {
   data() {
     return {
-      articles: [],
+      posts: [],
     };
   },
   methods: {
@@ -16,20 +16,20 @@ export default {
       const tokenHeader = new Headers();
       const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjIwLCJleHAiOjE2ODczNzMxMDB9.-mimtn4oM94oFgZiLUfQ_UGK6Ow01N3emR4ojKhjwZg';
       tokenHeader.append('Authorization', `Bearer ${token}`);
-      tokenHeader.append('content-type', 'application/json');
+     // tokenHeader.append('content-type', 'application/json');
       try {
-        const data = await fetch(`${apiBaseURL}/articles.json`,
+        const response = await fetch(`${apiBaseURL}/articles.json`,
             {
               method: 'GET',
               credentials: 'include',
               headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": 'application/json'
+                "Authorization": `Bearer ${token}`
               },
               mode: 'cors'
             });
-        this.articles = await data.json();
-        console.log(this.articles);
+        const finalRes = await response.json();
+        this.posts = finalRes;
+        console.log(this.posts);
       } catch (e) {
         console.error(e);
       }
@@ -46,10 +46,10 @@ export default {
     Articles
   </header>
 
-  {{this.articles}}
-
-    <div v-for="article in articles" >
-      {{article.title}}
+    <div v-for="array in this.posts" >
+      <div v-for="article in array">
+        Title: {{article.title}}
+      </div>
     </div>
 
 </template>
