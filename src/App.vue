b34fe511ad8423d7b237a47399f93e528a0fb951
Abby <amhoschouer@wpi.edu>
2023-06-19 11:38:22 -0400

    basic outline of functions

--- src/App.vue
+++ src/App.vue
@@ -17,19 +17,7 @@ export default {
 
 <template>
   <Articles v-if="page === 'indexed'"/>
-  <!--
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-  -->
 </template>
 
 
--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -9,6 +9,10 @@ import ArticleRow from "@/components/ArticleRow.vue";
 
 <script>
 const apiBaseURL = 'https://restfulapi--abbyhoosh.repl.co/proxy/5000';
+const tokenHeader = new Headers();
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjIwLCJleHAiOjE2ODczNzMxMDB9.-mimtn4oM94oFgZiLUfQ_UGK6Ow01N3emR4ojKhjwZg';
+tokenHeader.append('Authorization', `Bearer ${token}`);
+tokenHeader.append('content-type', 'application/json');
 
 export default {
   data() {
@@ -18,10 +22,6 @@ export default {
   },
   methods: {
     async getAllArticles() {
-      const tokenHeader = new Headers();
-      const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjIwLCJleHAiOjE2ODczNzMxMDB9.-mimtn4oM94oFgZiLUfQ_UGK6Ow01N3emR4ojKhjwZg';
-      tokenHeader.append('Authorization', `Bearer ${token}`);
-      tokenHeader.append('content-type', 'application/json');
       try {
         const response = await fetch(`${apiBaseURL}/articles.json`,
             {
@@ -32,10 +32,53 @@ export default {
             });
         this.posts = await response.json();
       } catch (e) {
-        console.error(e);
+        console.log(e);
+      }
+    },
+
+    async viewArticle(slug){
+      try {
+        const response = await fetch( `${apiBaseURL}/articles/view/${slug}.json`,
+            {
+              method: 'GET',
+              credentials: 'include',
+              headers: tokenHeader,
+              mode: 'cors'
+            });
+      } catch (e) {
+        console.log(e);
+      }
+    },
+
+    async editArticle(slug) {
+      try {
+        const response = await fetch( `${apiBaseURL}/articles/edit/${slug}.json`,
+            {
+              method: 'POST',
+              credentials: 'include',
+              headers: tokenHeader,
+              mode: 'cors'
+            });
+      } catch (e) {
+        console.log(e);
+      }
+    },
+
+    async deleteArticle(slug) {
+      try {
+        const response = await fetch( `${apiBaseURL}/articles/delete/${slug}.json`,
+            {
+              method: 'DELETE',
+              credentials: 'include',
+              headers: tokenHeader,
+              mode: 'cors'
+            });
+      } catch (e) {
+        console.log(e);
       }
     }
   },
+
   mounted() {
     this.getAllArticles();
   }
@@ -48,11 +91,21 @@ export default {
 
   <table v-for="array in this.posts">
     <tr>
-      <th class="left">Title</th>
+      <th class="title">Title</th>
       <th>Created</th>
       <th>Action</th>
     </tr>
-    <ArticleRow v-for="a in array" :a-title="a.title" :a-created= "a.created.format()" > </ArticleRow>
+
+    <tr v-for="a in array">
+      <td class="title">
+        <a> {{ a.title }} </a>
+      </td>
+      <td>{{ a.created }}</td>
+      <td>
+        <a>Edit </a>
+        <a>Delete</a>
+      </td>
+    </tr>
   </table>
 
 </template>
@@ -67,7 +120,13 @@ th {
   font-size: 25px;
 }
 
-.left{
+tr {
+  height: 40px;
+  font-size: 20px;
+  text-align: center;
+}
+
+.title {
   text-align: left;
   padding-left: 30px;
 }
