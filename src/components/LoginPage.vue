c4d17963cca9261d8ac01dc749d01bf101c447c0
Abby <amhoschouer@wpi.edu>
2023-06-20 13:23:45 -0400

    Create LoginPage.vue

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,30 @@
+
+<script>
+import Banner from "@/App.vue";
+import PageHeader from "@/components/PageHeader.vue";
+export default {
+  components: {PageHeader}
+}
+</script>
+
+<template>
+  <PageHeader header="Login"/>
+
+  <label for="user">Username</label>
+  <input type="text" id="user"/>
+  <label for="pass">Password</label>
+  <input type="password" id="pass"/>
+  <button>submit</button>
+
+</template>
+
+<style>
+label{
+  font-size: 25px;
+}
+input{
+  height: 50px;
+  width: 150px;
+}
+
+</style>
