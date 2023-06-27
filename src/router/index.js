6930b5447abf2cd6e4059a4cbce65ed92c37f588
Abby <amhoschouer@wpi.edu>
2023-06-27 15:13:17 -0400

    add route paths

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import {createRouter, createWebHistory} from "vue-router";
 import LoginPage from "@/views/LoginPage.vue";
-//import pages from view once they are moved there
 
 const routes = [
     {path: '/', name: 'login', component: LoginPage},
     {path: '/Articles', name: 'Articles', component: ()=> import('@/views/Articles.vue')},
-    {path: '/Articles/View/:slug', name: 'ViewPage', component:()=> import('@/views/ViewPage.vue')}
+    {path: '/Articles/View', name: 'ViewPage', component:()=> import('@/views/ViewPage.vue')},
+    {path: '/Articles/Add', name: 'AddPage', component:()=> import('@/views/AddPage.vue')},
+    {path: '/Articles/Edit', name: 'EditPage', component:()=> import('@/views/EditPage.vue')}
 ]
 
 const router = createRouter({
